2fd4a95c64bb934c61194c3fff0a66ed2f7e3dde
dukesrg <dukesrg@gmail.com>
2019-12-18 01:33:51 +0300

    Mix modes updated, dynamic length limit

--- platform/nutekt-digital/looper/README.md
+++ platform/nutekt-digital/looper/README.md
@@ -3,5 +3,8 @@
 This is a Kaossilator-style stereo loop recorded for revfx slot.
 
 Parameters:
-* time (A knob) : loop length linked to current BPM and sticked to 1/16, 1/8, 1/4, 1/2, 1, 2, 4 beats
-* depth (B knob) : play (<=50) / record (>50) trigger
+* time (A knob) : loop length linked to current BPM and sticked to 1/16, 1/8, 1/4, 1/2, 1, 2, 4, 8, 16 beats. Length reduced dynamically depending on BPM change due to available SDRAM size limit.
+* depth (B knob) :
+  * 0...33 : overwrite - 50/50 mixed input and delay buffer passed to the output, input rewrites the delay buffer.
+  * 34...66 : play - 50/50 mixed input and delay buffer passed to the output, delay buffer untouched.
+  * 67...100 : overdub - 50/50 mixed input and delay buffer passed to the output, input added to the delay buffer.
--- platform/nutekt-digital/looper/looper.cpp
+++ platform/nutekt-digital/looper/looper.cpp
@@ -9,43 +9,72 @@
  */
 
 #include "fx_api.h"
+#include "float_math.h"
 #include "osc_api.h"
 #include "userrevfx.h"
 
 #include "delayline.hpp"
 
-#define MIN_BPM 56.f
-//#define MIN_BEATS 1.f/16
-#define MAX_BEATS 4.f
-//Min BPM for 16 beats stereo loop: sample_rate * sizeof(float) * 2_channels * 16_beats * 60_seconds / 2432K = 148.026 BPM
-//or 74.01 for 8 beats
-//or 37.01 for 4 beats
-//Really, KORG? :(
-#define bufsize (size_t)(k_samplerate * 60 * MAX_BEATS / MIN_BPM)
+#define MAX_MEM (2432 * 1024)
+#define BUF_SIZE (MAX_MEM / (sizeof(float) * 2))
+#define BEAT_STEPS 9 // 1/16 to 16
+
+enum {
+  mix_mode_overwrite = 0,
+  mix_mode_play,
+  mix_mode_overdub,
+  mix_mode_count
+};
 
 static dsp::DelayLine s_delay;
 
-static __sdram float s_delay_ram[bufsize * 2];
+static __sdram float s_delay_ram[BUF_SIZE * 2];
 
-static float s_len;
-static bool s_record;
+static int s_beats;
+static int s_mix_mode;
 
 void REVFX_INIT(uint32_t platform, uint32_t api)
 {
-  s_delay.setMemory(s_delay_ram, bufsize * 2);
-  s_len = bufsize;
-  s_record = false;
+  s_delay.setMemory(s_delay_ram, BUF_SIZE * 2);
+  s_beats = BEAT_STEPS - 1;
+  s_mix_mode = mix_mode_play;
 }
 
 void REVFX_PROCESS(float *xn, uint32_t frames)
 {
   float * __restrict x = xn;
   const float * x_e = x + 2 * frames;
-  for (; x != x_e; x++) {
-    *x = (*x + s_delay.read(s_len)) * .5f;
-    if (s_record) {
+
+// adjust length to the buffer limit
+  int beats = s_beats;
+  float val = (k_samplerate * 60.f / 4.f) / fx_get_bpmf();
+  float len;
+  do {
+    len = val * (1 << (beats--));
+  } while (len > BUF_SIZE);
+
+  switch(s_mix_mode) {
+  case mix_mode_overwrite:
+    for (; x != x_e; x++) {
+      val = s_delay.read(len);
       s_delay.write(*x);
+      *x = (*x + val) * .5f;
     }
+    break;
+  case mix_mode_play:
+    for (; x != x_e; x++) {
+      *x = (*x + s_delay.read(len)) * .5f;
+    }
+    break;
+  case mix_mode_overdub:
+    for (; x != x_e; x++) {
+      val = *x + s_delay.read(len);
+      s_delay.write(val);
+      *x = val * .5f;
+    }
+    break;
+  default:
+    break;
   }
 }
 
@@ -53,11 +82,11 @@ void REVFX_PARAM(uint8_t index, int32_t value)
 {
   const float valf = q31_to_f32(value);
   switch (index) {
-  case k_user_revfx_param_time: // time stick to BPM from 1/16 to MAX_BEATS
-    s_len = k_samplerate * 60.f * (1 >> (int)(valf * 7.f)) / (fx_get_bpmf() * 4);
+  case k_user_revfx_param_time: // beats length factor
+    s_beats = clipmaxf(si_floorf(valf * BEAT_STEPS), BEAT_STEPS - 1);
     break;
-  case k_user_revfx_param_depth: // mix or record
-    s_record = valf > 0.5f;
+  case k_user_revfx_param_depth: // record mode
+    s_mix_mode = clipmaxf(si_floorf(valf * mix_mode_count), mix_mode_count - 1);
     break;
   case k_user_revfx_param_shift_depth:
     break;
@@ -65,4 +94,3 @@ void REVFX_PARAM(uint8_t index, int32_t value)
     break;
   }
 }
-
