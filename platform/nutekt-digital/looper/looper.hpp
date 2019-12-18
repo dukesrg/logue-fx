f3d3fffbbf319e3f106d948f3a7e533bf78c8672
dukesrg <dukesrg@gmail.com>
2019-12-18 17:09:43 +0300

    revert back to DelayLine; mono mode implemented

--- platform/nutekt-digital/looper/looper.hpp
+++ platform/nutekt-digital/looper/looper.hpp
@@ -10,6 +10,7 @@
  */
 
 #include "fx_api.h"
+//#include "fixed_math.h"
 #include "float_math.h"
 #include "osc_api.h"
 #include "userrevfx.h"
@@ -17,7 +18,7 @@
 #include "delayline.hpp"
 
 #define MAX_MEM (2400 * 1024) //exact BPM limits, 32K reserved
-#define BUF_SIZE (MAX_MEM / sizeof(f32pair_t))
+#define BUF_SIZE (MAX_MEM / (sizeof(float) * 2))
 #define BEAT_MAX 16 // maximum loop length, beats
 #define BEAT_MIN 16 // minimum loop length, beat fractions
 #define BEAT_STEPS 9 // = log2(BEAT_MAX * BEAT_MIN) + 1
@@ -36,10 +37,22 @@ enum {
   record_format_mono_packed,
   record_format_count
 };
+/*
+typedef struct {
+  q15_t a;
+  q15_t b;
+} q15pair_t;
 
-static dsp::DualDelayLine s_delay;
+typedef union {
+  float f;
+  q15pair_t q;
+} q15f32_t;
 
-static __sdram f32pair_t s_delay_ram[BUF_SIZE];
+#define f32x2_to_q15(f1, f2) (q15pair_t){f32_to_q15(f1), f32_to_q15(f2)}
+*/
+static dsp::DelayLine s_delay;
+
+static __sdram float s_delay_ram[BUF_SIZE * 2];
 
 static int s_beats;
 static int s_mix_mode;
@@ -48,7 +61,7 @@ static int s_record_format;
 static inline __attribute__((optimize("Ofast"),always_inline))
 void looper_init(uint32_t platform, uint32_t api)
 {
-  s_delay.setMemory(s_delay_ram, BUF_SIZE);
+  s_delay.setMemory(s_delay_ram, BUF_SIZE * 2);
   s_beats = BEAT_STEPS - 1;
   s_mix_mode = mix_mode_play;
   s_record_format = record_format_stereo;
@@ -68,26 +81,65 @@ void looper_process(float *xn, uint32_t frames)
     len = val * (1 << (beats--));
   } while (len > BUF_SIZE);
 
-  f32pair_t valfp;
-
-  switch(s_mix_mode) {
-  case mix_mode_overwrite:
-    for (; x != x_e; x+=2) {
-      valfp = *(f32pair_t *)x;
-      *(f32pair_t *)x = f32pair_linint(.5f, valfp, s_delay.read(len));
-      s_delay.write(valfp);
+  float valf, valf2;
+  switch (s_record_format) {
+  case record_format_stereo:
+  case record_format_stereo_packed: //ToDo
+    len *= 2;
+    switch (s_mix_mode) {
+    case mix_mode_overwrite:
+      for (; x != x_e; x) {
+        valf = s_delay.read(len);
+        s_delay.write(*x);
+        *(x++) = (*x + valf) * .5f;
+      }
+      break;
+    case mix_mode_play:
+      for (; x != x_e;) {
+        valf = s_delay.read(len);
+        *(x++) = (*x + valf) * .5f;
+      }
+      break;
+    case mix_mode_overdub:
+      for (; x != x_e; x++) {
+        valf = s_delay.read(len);
+        *(x++) = (*x + valf) * .5f;
+        s_delay.write(valf);
+      }
+      break;
+    default:
+      break;
     }
     break;
-  case mix_mode_play:
-    for (; x != x_e; x+=2) {
-      *(f32pair_t *)x = f32pair_linint(.5f, *(f32pair_t *)x, s_delay.read(len));
-    }
-    break;
-  case mix_mode_overdub:
-    for (; x != x_e; x+=2) {
-      valfp = f32pair_add(*(f32pair_t *)x, s_delay.read(len));
-      *(f32pair_t *)x = f32pair_mulscal(valfp, .5f);
-      s_delay.write(valfp);
+  case record_format_mono:
+  case record_format_mono_packed: //ToDo
+    switch (s_mix_mode) {
+    case mix_mode_overwrite:
+      for (; x != x_e; x) {
+        valf = s_delay.read(len);
+        s_delay.write((*x + x[1]) * .5f);
+        *(x++) = (*x + valf) * .5f;
+        *(x++) = (*x + valf) * .5f;
+      }
+      break;
+    case mix_mode_play:
+      for (; x != x_e;) {
+        valf = s_delay.read(len);
+        *(x++) = (*x + valf) * .5f;
+        *(x++) = (*x + valf) * .5f;
+      }
+      break;
+    case mix_mode_overdub:
+      for (; x != x_e; x) {
+        valf = s_delay.read(len) ;
+        valf += (*x + x[1]) * .5f;
+        *(x++) = (*x + valf) * .5f;
+        *(x++) = (*x + valf) * .5f;
+        s_delay.write(valf);
+      }
+      break;
+    default:
+      break;
     }
     break;
   default:
